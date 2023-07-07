94f20f1e64974c19aa22cf5b23344e90acba7059
Philip Vejre <philip@vejre.blue>
2023-07-07 14:07:07 +0200

    initial commit

---
+++ lib/src/main/kotlin/aegis/Aegis.kt
@@ -0,0 +1,262 @@
+package aegis
+
+const val KEYSIZE = 16
+const val NONCESIZE = 16
+
+private val CONST0: Array<UInt> = arrayOf(0x02010100u, 0xd080503u, 0x59372215u, 0x6279e990u)
+private val CONST1: Array<UInt> = arrayOf(0x55183ddbu, 0xf12fc26du, 0x42311120u, 0xdd28b573u)
+private var t = Array<Array<UInt>>(4) { Array<UInt>(256) { 0u } }
+
+private fun init() {
+  if (t[0][0] != 0u) {
+    return
+  }
+
+  var d = Array(256) { _ -> 0u}
+  var th = Array(256) { _ -> 0u}
+
+  for (i in 0u until 256u) {
+    val v = (i shl 1) xor (i shr 7)*283u
+    d[i.toInt()] = v
+    th[(v xor i).toInt()] = i
+  }
+
+  var x: UInt = 0u
+  var xInv: UInt = 0u
+
+  for (j in 0 until 256) {
+    var s = xInv xor (xInv shl 1) xor (xInv shl 2) xor (xInv shl 3) xor (xInv shl 4)
+    s = (s shr 8) xor (s and 255u) xor 99u
+    val x2 = d[x.toInt()]
+    var tEnc = (d[s.toInt()]*0x101u) xor (s*0x1010100u)
+
+    for (i in 0 until 4) {
+      tEnc = (tEnc shl 24) xor (tEnc shr 8)
+      t[i][x.toInt()] = tEnc
+    }
+
+    if (x2 == 0u) {
+      x = x xor 1u
+    } else {
+      x = x xor x2
+    }
+    if (th[xInv.toInt()] == 0u) {
+      xInv = 1u
+    } else {
+      xInv = th[xInv.toInt()]
+    }
+  }
+
+  // Swap byte order
+  for (i in 0 until 4) {
+    for (j in 0 until 256) {
+      val b = t[i][j]
+      t[i][j] = ((b shl 24) and 0xff000000u) xor ((b shl 8) and 0x00ff0000u) xor ((b shr 8) and 0x0000ff00u) xor ((b shr 24) and 0x000000ffu)
+    }
+  }
+}
+
+private fun uIntArrayToUByteArray(input: Array<UInt>): Array<UByte> {
+  val output = Array<UByte>(4 * input.size){ 0u }
+
+  for ((i, v) in input.withIndex()) {
+    output[4*i+0] = v.toUByte()
+    output[4*i+1] = (v shr 8).toUByte()
+    output[4*i+2] = (v shr 16).toUByte()
+    output[4*i+3] = (v shr 24).toUByte()
+  }
+
+  return output
+}
+
+private fun uByteArrayToUIntArray(input: Array<UByte>): Array<UInt> {
+  var size = input.size / 4
+  if (input.size % 4 != 0) {
+    size++
+  }
+  val output = Array<UInt>(size){ 0u }
+
+  for ((i, v) in input.withIndex()) {
+    output[i/4] = output[i/4] xor (v.toUInt() shl 8*(i%4))
+  }
+
+  return output
+}
+
+private fun xor128Into(value: Array<UInt>, dst: Array<UInt>, dstOffset: Int) {
+  dst[dstOffset + 0] = dst[dstOffset + 0] xor value[0]
+  dst[dstOffset + 1] = dst[dstOffset + 1] xor value[1]
+  dst[dstOffset + 2] = dst[dstOffset + 2] xor value[2]
+  dst[dstOffset + 3] = dst[dstOffset + 3] xor value[3]
+}
+
+private fun extract(a: Array<UInt>, b: Array<UInt>, c: Array<UInt>, d: Array<UInt>): Array<UInt> {
+  return arrayOf(
+    (a[0] and b[0]) xor c[0] xor d[0],
+    (a[1] and b[1]) xor c[1] xor d[1],
+    (a[2] and b[2]) xor c[2] xor d[2],
+    (a[3] and b[3]) xor c[3] xor d[3],
+  )
+}
+
+private fun zeroPad(data: Array<UByte>): Array<UInt> {
+  if (data.size % 32 == 0) {
+    return uByteArrayToUIntArray(data)
+  }
+  val padLen = 32 - data.size % 32
+  return uByteArrayToUIntArray(data + Array<UByte>(padLen){ 0u })
+}
+
+private fun round(src: Array<UInt>, key: Array<UInt>, dst: Array<UInt>) {
+  val src8 = uIntArrayToUByteArray(src)
+  val x0 = t[0][src8[0].toInt()]  xor t[1][src8[5].toInt()]  xor t[2][src8[10].toInt()] xor t[3][src8[15].toInt()]
+  val x1 = t[0][src8[4].toInt()]  xor t[1][src8[9].toInt()]  xor t[2][src8[14].toInt()] xor t[3][src8[3].toInt()]
+  val x2 = t[0][src8[8].toInt()]  xor t[1][src8[13].toInt()] xor t[2][src8[2].toInt()]  xor t[3][src8[7].toInt()]
+  val x3 = t[0][src8[12].toInt()] xor t[1][src8[1].toInt()]  xor t[2][src8[6].toInt()]  xor t[3][src8[11].toInt()]
+
+  dst[0] = key[0] xor x0
+  dst[1] = key[1] xor x1
+  dst[2] = key[2] xor x2
+  dst[3] = key[3] xor x3
+}
+
+private fun tagValid(a: Array<UByte>, b: Array<UByte>): Boolean {
+  var equal = true
+  for (i in 0 until a.size) {
+    equal = equal and (a[i] == b[i])
+  }
+  return equal
+}
+
+class Aegis128L {
+  private var s: Array<Array<UInt>> = Array<Array<UInt>>(8) { Array<UInt>(4) { 0u } }
+
+  init {
+    init()
+  }
+
+  private fun update(a: Array<UInt>, aOffset: Int, b: Array<UInt>, bOffset: Int) {
+    val t = s[7].copyOf()
+
+    round(this.s[6], this.s[7], this.s[7])
+    round(this.s[5], this.s[6], this.s[6])
+    round(this.s[4], this.s[5], this.s[5])
+
+    this.s[4][0] = this.s[4][0] xor b[bOffset+0]
+    this.s[4][1] = this.s[4][1] xor b[bOffset+1]
+    this.s[4][2] = this.s[4][2] xor b[bOffset+2]
+    this.s[4][3] = this.s[4][3] xor b[bOffset+3]
+
+    round(this.s[3], this.s[4], this.s[4])
+    round(this.s[2], this.s[3], this.s[3])
+    round(this.s[1], this.s[2], this.s[2])
+    round(this.s[0], this.s[1], this.s[1])
+
+    this.s[0][0] = this.s[0][0] xor a[aOffset+0]
+    this.s[0][1] = this.s[0][1] xor a[aOffset+1]
+    this.s[0][2] = this.s[0][2] xor a[aOffset+2]
+    this.s[0][3] = this.s[0][3] xor a[aOffset+3]
+
+    round(t, this.s[0], this.s[0])
+  }
+
+  private fun process(key: Array<UByte>, nonce: Array<UByte>, msg: Array<UByte>, data: Array<UByte>, decrypt: Boolean): Array<UByte> {
+    check(key.size == KEYSIZE) { "invalid key size" }
+    check(nonce.size == NONCESIZE) { "invalid nonce size" }
+
+    val msgLen = msg.size
+    val dataLen = data.size
+
+    val keyArray = uByteArrayToUIntArray(key)
+    val nonceArray = uByteArrayToUIntArray(nonce)
+    var msgArray = zeroPad(msg)
+    val dataArray = zeroPad(data)
+
+    // Initialize
+    this.s = arrayOf(
+      keyArray.copyOf(),
+      CONST1.copyOf(),
+      CONST0.copyOf(),
+      CONST1.copyOf(),
+      keyArray.copyOf(),
+      keyArray.copyOf(),
+      keyArray.copyOf(),
+      keyArray.copyOf(),
+    )
+
+    xor128Into(nonceArray, this.s[0], 0)
+    xor128Into(nonceArray, this.s[4], 0)
+    xor128Into(CONST0, this.s[5], 0)
+    xor128Into(CONST1, this.s[6], 0)
+    xor128Into(CONST0, this.s[7], 0)
+
+    for (i in 0 until 10) {
+      update(nonceArray, 0, keyArray, 0)
+    }
+
+    // Process associated data
+    val dataBlocks = dataArray.size/8
+    for (i in 0 until dataBlocks) {
+      update(dataArray, i*8, dataArray, i*8+4)
+    }
+
+    // Process plaintext
+    val msgBlocks = msgArray.size/8
+    for (i in 0 until msgBlocks) {
+      val z0 = extract(this.s[2], this.s[3], this.s[1], this.s[6])
+      val z1 = extract(this.s[6], this.s[7], this.s[5], this.s[2])
+
+      if (!decrypt) {
+        update(msgArray, i*8, msgArray, i*8+4)
+      }
+
+      xor128Into(z0, msgArray, i*8)
+      xor128Into(z1, msgArray, i*8+4)
+
+      if (decrypt) {
+        // Recreate zero padding if needed
+        if (msgLen < (i+1)*32) {
+          // TODO: This is very inefficient
+          var x = uIntArrayToUByteArray(msgArray)
+          for (j in msgLen until x.size) {
+            x[j] = 0u
+          }
+          msgArray = uByteArrayToUIntArray(x)
+        }
+        update(msgArray, i*8, msgArray, i*8+4)
+      }
+    }
+    var x = uIntArrayToUByteArray(msgArray)
+    for (i in 0 until msg.size) {
+      msg[i] = x[i]
+    }
+
+    // Generate tag. Note that the encoding here restricts data and message length to
+    // 2^32 instead of 2^64 as per the specification.
+    var tag: Array<UInt> = arrayOf(dataLen.toUInt()*8u, 0u, msgLen.toUInt()*8u, 0u)
+    xor128Into(this.s[2], tag, 0)
+    for (i in 0 until 7) {
+      update(tag, 0, tag, 0)
+    }
+    tag = this.s[0]
+    xor128Into(this.s[1], tag, 0)
+    xor128Into(this.s[2], tag, 0)
+    xor128Into(this.s[3], tag, 0)
+    xor128Into(this.s[4], tag, 0)
+    xor128Into(this.s[5], tag, 0)
+    xor128Into(this.s[6], tag, 0)
+    return uIntArrayToUByteArray(tag)
+  }
+
+  fun seal(key: Array<UByte>, nonce: Array<UByte>, msg: Array<UByte>, data: Array<UByte>): Array<UByte> {
+    return this.process(key, nonce, msg, data, false)
+  }
+
+  fun open(key: Array<UByte>, nonce: Array<UByte>, msg: Array<UByte>, data: Array<UByte>, tag: Array<UByte>) {
+    val tagDecrypt = this.process(key, nonce, msg, data, true)
+    if (!tagValid(tag, tagDecrypt)) {
+      msg.fill(0u)
+      throw Exception("invalid tag")
+    }
+  }
+}
---
+++ lib/src/test/kotlin/aegis/AegisTest.kt
@@ -0,0 +1,149 @@
+package aegis
+
+import kotlin.test.Test
+import kotlin.test.*
+import kotlin.time.measureTime
+import kotlin.time.DurationUnit.MILLISECONDS
+
+fun String.hexToArray(): Array<UByte> {
+  check(length % 2 == 0) { "Must have an even length" }
+
+  return Array<UByte>(length / 2) {
+    Integer.parseInt(this, it * 2, (it + 1) * 2, 16).toUByte()
+  }
+}
+
+class AegisTest {
+  @Test fun sanity() {
+    val key = "000102030405060708090a0b0c0d0e0f".hexToArray();
+    val nonce = "101112131415161718191a1b1c1d1e1f".hexToArray();
+    val data = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829".hexToArray();
+    val plaintext = "101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637".hexToArray();
+    val msg = plaintext.copyOf();
+
+    val c = Aegis128L()
+
+    val tagEncrypt = c.seal(key, nonce, msg, data);
+    assertFalse(msg contentEquals plaintext)
+
+    try {
+      c.open(key, nonce, msg, data, tagEncrypt);
+    } catch (e: Exception) {
+      fail("open failed", e)
+    }
+
+    assertTrue(msg contentEquals plaintext)
+  }
+
+  @Test fun invalidTag() {
+    val key = "000102030405060708090a0b0c0d0e0f".hexToArray();
+    val nonce = "101112131415161718191a1b1c1d1e1f".hexToArray();
+    val data = "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829".hexToArray();
+    val plaintext = "101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637".hexToArray();
+    val msg = plaintext.copyOf();
+
+    val c = Aegis128L()
+
+    val tagEncrypt = c.seal(key, nonce, msg, data);
+    tagEncrypt[0] = tagEncrypt[0] xor 1u;
+
+    assertFails({ c.open(key, nonce, msg, data, tagEncrypt) })
+    assertTrue(msg.all { it == 0u.toUByte()} )
+  }
+
+  @Test fun knownAnswers() {
+    val key = arrayOf(
+      "10010000000000000000000000000000".hexToArray(),
+      "10010000000000000000000000000000".hexToArray(),
+      "10010000000000000000000000000000".hexToArray(),
+      "10010000000000000000000000000000".hexToArray(),
+      "10010000000000000000000000000000".hexToArray(),
+    )
+    val nonce = arrayOf(
+      "10000200000000000000000000000000".hexToArray(),
+      "10000200000000000000000000000000".hexToArray(),
+      "10000200000000000000000000000000".hexToArray(),
+      "10000200000000000000000000000000".hexToArray(),
+      "10000200000000000000000000000000".hexToArray(),
+    )
+    val data = arrayOf(
+      "".hexToArray(),
+      "".hexToArray(),
+      "0001020304050607".hexToArray(),
+      "0001020304050607".hexToArray(),
+      "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f20212223242526272829".hexToArray(),
+    )
+    val msg = arrayOf(
+      "00000000000000000000000000000000".hexToArray(),
+      "".hexToArray(),
+      "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f".hexToArray(),
+      "000102030405060708090a0b0c0d".hexToArray(),
+      "101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637".hexToArray(),
+    )
+    val ct = arrayOf(
+      "c1c0e58bd913006feba00f4b3cc3594e".hexToArray(),
+      "".hexToArray(),
+      "79d94593d8c2119d7e8fd9b8fc77845c5c077a05b2528b6ac54b563aed8efe84".hexToArray(),
+      "79d94593d8c2119d7e8fd9b8fc77".hexToArray(),
+      "b31052ad1cca4e291abcf2df3502e6bdb1bfd6db36798be3607b1f94d34478aa7ede7f7a990fec10".hexToArray(),
+    )
+    val tag = arrayOf(
+      "abe0ece80c24868a226a35d16bdae37a".hexToArray(),
+      "c2b879a67def9d74e6c14f708bbcc9b4".hexToArray(),
+      "cc6f3372f6aa1bb82388d695c3962d9a".hexToArray(),
+      "5c04b3dba849b2701effbe32c7f0fab7".hexToArray(),
+      "7542a745733014f9474417b337399507".hexToArray(),
+    )
+
+    val c = Aegis128L()
+    for (i in 0 until key.size) {
+      val tagEnc = c.seal(key[i], nonce[i], msg[i], data[i])
+      assertTrue(msg[i] contentEquals ct[i], "($i) wrong ciphertext: ${msg[i].contentToString()} != ${ct[i].contentToString()}")
+      assertTrue(tag[i] contentEquals tagEnc, "($i) wrong tag: ${tag[i].contentToString()} != ${tagEnc.contentToString()}")
+    }
+  }
+
+  @OptIn(kotlin.time.ExperimentalTime::class)
+  @Test fun benchmarkEncrypt() {
+    val key = "10010000000000000000000000000000".hexToArray();
+    val nonce = "10000200000000000000000000000000".hexToArray();
+    val data = Array<UByte>(0){ 0u };
+    val msg = Array<UByte>(102400){ 0u };
+    val n = 1000;
+    val c = Aegis128L()
+
+    val d = measureTime({
+      for (i in 0 until n) {
+        c.seal(key, nonce, msg, data);
+      }
+    }).toDouble(MILLISECONDS)
+
+    println("Aegis encrypt: Total time: ${d} ms")
+    println("Aegis encrypt: Time per op: ${d/n} ms")
+    println("Aegis encrypt: Bytes per second: ${(n*msg.size)/(d/n)}")
+    println("Aegis encrypt: Nanoseconds per byte: ${(d*1000000)/(n*msg.size)}")
+  }
+
+  @OptIn(kotlin.time.ExperimentalTime::class)
+  @Test fun benchmarkDecrypt() {
+    val key = "10010000000000000000000000000000".hexToArray();
+    val nonce = "10000200000000000000000000000000".hexToArray();
+    val data = Array<UByte>(0){ 0u };
+    val msg = Array<UByte>(102400){ 0u };
+    val n = 1000;
+    val c = Aegis128L()
+    val tag = c.seal(key, nonce, msg, data);
+
+    val d = measureTime({
+      for (i in 0 until n) {
+        val ciphertext = msg.copyOf()
+        c.open(key, nonce, ciphertext, data, tag);
+      }
+    }).toDouble(MILLISECONDS)
+
+    println("Aegis decrypt: Total time: ${d} ms")
+    println("Aegis decrypt: Time per op: ${d/n} ms")
+    println("Aegis decrypt: Bytes per second: ${(n*msg.size)/(d/n)}")
+    println("Aegis decrypt: Nanoseconds per byte: ${(d*1000000)/(n*msg.size)}")
+  }
+}
